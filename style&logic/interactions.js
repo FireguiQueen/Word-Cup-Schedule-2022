f4cf8e4e900c432878eea8393bfde04e77ddbe63
Guilherme <Fireguiquenbr@gmail.com>
2022-12-11 13:49:15 -0300

    i

---
+++ style&logic/interactions.js
@@ -0,0 +1,28 @@
+var calendar = document.querySelector('#app')
+calendar.innerHTML = `
+<main id="calendar"> 
+            <div class="card">
+                <h2> 11/24 <span class="DayOfTheWeek">thursday</span> </h2>
+                <ul> 
+                    <li>
+                        <img src="./assets/icons/brazil.svg" alt="Brazil's flag icon" />  
+                        <strong> 06:30 </strong> 
+                        <img src="./assets/icons/serbia.svg" alt="Seria's flag icon" /> 
+                    </li>  
+                    <li>
+                        <img src="./assets/icons/brazil.svg" alt="Brazil's flag icon" />  
+                        <strong> 06:30 </strong> 
+                        <img src="./assets/icons/serbia.svg" alt="Seria's flag icon" /> 
+                    </li>   
+                    <li>
+                        <img src="./assets/icons/brazil.svg" alt="Brazil's flag icon" />  
+                        <strong> 06:30 </strong> 
+                        <img src="./assets/icons/serbia.svg" alt="Seria's flag icon" /> 
+                    </li>   
+                </ul> 
+            </div> 
+        </main> 
+
+
+
+`
