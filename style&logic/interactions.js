cc1e71e5d14ef0951008df951235c3b410b53c09
Guilherme <Fireguiquenbr@gmail.com>
2022-12-11 22:45:45 -0300

    Update

--- style&logic/interactions.js
+++ style&logic/interactions.js
@@ -1,28 +1,25 @@
-var calendar = document.querySelector('#app')
+function CreatSchedule(){
+    return`
+        <div class="card"> 
+        <h2> 11/24 <span class="DayOfTheWeek">thursday</span></h2>
+            <ul> 
+                <li>
+                    <img src="./assets/icons/brazil.svg" alt="Brazil's flag icon" />  
+                    <strong> 06:30 </strong> 
+                    <img src="./assets/icons/serbia.svg" alt="Seria's flag icon" /> 
+                </li>
+            </ul> 
+        </div>
+    `
+}
+
+
+
+var calendar = document.querySelector('#cartoes')
+
 calendar.innerHTML = `
-<main id="calendar"> 
-            <div class="card">
-                <h2> 11/24 <span class="DayOfTheWeek">thursday</span> </h2>
-                <ul> 
-                    <li>
-                        <img src="./assets/icons/brazil.svg" alt="Brazil's flag icon" />  
-                        <strong> 06:30 </strong> 
-                        <img src="./assets/icons/serbia.svg" alt="Seria's flag icon" /> 
-                    </li>  
-                    <li>
-                        <img src="./assets/icons/brazil.svg" alt="Brazil's flag icon" />  
-                        <strong> 06:30 </strong> 
-                        <img src="./assets/icons/serbia.svg" alt="Seria's flag icon" /> 
-                    </li>   
-                    <li>
-                        <img src="./assets/icons/brazil.svg" alt="Brazil's flag icon" />  
-                        <strong> 06:30 </strong> 
-                        <img src="./assets/icons/serbia.svg" alt="Seria's flag icon" /> 
-                    </li>   
-                </ul> 
-            </div> 
-        </main> 
-
-
-
+<main id="cards"> 
+    ${CreatSchedule()}
+    ${CreatSchedule()}
+</main> 
 `
