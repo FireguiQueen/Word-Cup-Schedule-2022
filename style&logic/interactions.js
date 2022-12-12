fdff2c9a3b974ce88b32b24f206284f3c1f5acf3
Guilherme <Fireguiquenbr@gmail.com>
2022-12-12 01:22:22 -0300

    Update

--- style&logic/interactions.js
+++ style&logic/interactions.js
@@ -1,15 +1,21 @@
-function CreatSchedule(date, day, team1, time, team2){
+function CreatSchedule(date, day, games){
     return`
         <div class="card"> 
         <h2> ${date} <span class="DayOfTheWeek">${day}</span></h2>
             <ul> 
-                <li>
-                    <img src="./assets/icons/${team1}.svg" alt="${team1}'s flag icon" />  
-                    <strong> 06:30 </strong> 
-                    <img src="./assets/icons/${team2}.svg" alt="${team2}'s flag icon" /> 
-                </li>
+                ${games}
             </ul> 
         </div>
+    `
+}
+
+function CreatGames(team1, time, team2){
+    return`
+        <li>
+            <img src="./assets/icons/${team1}.svg" alt="${team1}'s flag icon" />  
+            <strong> 06:30 </strong> 
+            <img src="./assets/icons/${team2}.svg" alt="${team2}'s flag icon" /> 
+        </li>
     `
 }
 
@@ -21,7 +27,7 @@ var calendar = document.querySelector('#cartoes')
 calendar.innerHTML = `
 <main id="cards"> 
 
-    ${CreatSchedule('11/11', 'segunda', 'brazil', '11:30', 'serbia')}
-    ${CreatSchedule('11/11', 'segunda', 'brazil', '11:30', 'serbia')}
+    ${CreatSchedule('11/11', 'monday', CreatGames('cameroon', '12:20', 'uruguai'))}
+    ${CreatSchedule('11/11', 'monday', CreatGames('brazil', '16:30', 'switzerland') + CreatGames('brazil', '16:30', 'switzerland'))}
 </main> 
 `
