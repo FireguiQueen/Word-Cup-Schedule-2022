e3801b3516d0b9a0f0648550b16f233cafb6728e
Guilherme <Fireguiquenbr@gmail.com>
2022-12-13 01:51:42 -0300

    Update

--- style&logic/interactions.js
+++ style&logic/interactions.js
@@ -1,6 +1,9 @@
+let atraso = -0.2
+
 function CreatSchedule(date, day, games){
+    atraso += 0.2;
     return`
-        <div class="card"> 
+        <div class="card" style="animation-delay: ${atraso}s"> 
         <h2> ${date} <span class="DayOfTheWeek">${day}</span></h2>
             <ul> 
                 ${games}
@@ -13,21 +16,27 @@ function CreatGames(team1, time, team2){
     return`
         <li>
             <img src="./assets/icons/${team1}.svg" alt="${team1}'s flag icon" />  
-            <strong> 06:30 </strong> 
+            <strong> ${time} </strong> 
             <img src="./assets/icons/${team2}.svg" alt="${team2}'s flag icon" /> 
         </li>
     `
 }
 
 
-
 var calendar = document.querySelector('#cartoes')
 
-// DATE | DAY | TEAM 1 | TIME | TEAM 2 
-calendar.innerHTML = `
-<main id="cards"> 
+calendar.innerHTML = 
+CreatSchedule('11/12', 'segunda', CreatGames('brazil', '15:20', 'uruguai') + CreatGames('switzerland', '12:20', 'portugal')) +
+CreatSchedule('11/12', 'segunda', CreatGames('brazil', '15:20', 'uruguai')) + 
+CreatSchedule('11/12', 'segunda', CreatGames('brazil', '15:20', 'uruguai')) + 
+CreatSchedule('11/12', 'segunda', CreatGames('brazil', '15:20', 'uruguai'))
 
-    ${CreatSchedule('11/11', 'monday', CreatGames('cameroon', '12:20', 'uruguai'))}
-    ${CreatSchedule('11/11', 'monday', CreatGames('brazil', '16:30', 'switzerland') + CreatGames('brazil', '16:30', 'switzerland'))}
-</main> 
-`
+// CreatSchedule('11/11', 'monday', CreatGames('cameroon', '16:30', 'uruguai')) +
+// CreatSchedule('11/11', 'monday', CreatGames('brazil', '16:30', 'switzerland') + CreatGames('brazil', '16:30', 'switzerland'))
+
+
+
+function ChangeColor(){ 
+    var ide = document.getElementsByTagName('main')[0];
+    ide.setAttribute('class', 'blue');
+}
